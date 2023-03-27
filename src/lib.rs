9c15494b4c7f074b7e4d08abb3bc22291eb28b6c
Federico Aguzzi <federico.aguzzi@icloud.com>
2023-03-27 21:55:33 +0200

    Fixed errors, server now operational

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
 use anyhow::Result;
-
+use cap_std::fs::File as CapStdFile;
+use std::io::prelude::*;
 use wasmtime::*;
 use wasmtime_wasi::*;
 
-pub fn wasm_loader(module: String) -> Result<()> {
+pub fn wasm_loader(module: String) -> Result<String> {
     // Define the WASI functions globally on the `Config`.
     let engine = Engine::default();
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
 
+    // Instantiating a stdout file for later usage
+    let temp_file = tempfile::tempfile()?;
+    let mut capstd_file = CapStdFile::from_std(temp_file);
+    let wasmtime_file = wasmtime_wasi::file::File::from_cap_std(capstd_file.try_clone()?);
+
     // Create a WASI context and put it in a Store; all instances in the store
     // share this context. `WasiCtxBuilder` provides a number of ways to
     // configure what the target program will have access to.
     let wasi = WasiCtxBuilder::new()
-        .inherit_stdio()
-        .inherit_args()?
+        .stdout(Box::new(wasmtime_file))
         .build();
     let mut store = Store::new(&engine, wasi);
 
@@ -30,5 +35,12 @@ pub fn wasm_loader(module: String) -> Result<()> {
     let instance_main = instance.get_typed_func::<(), ()>(&mut store, "_start")?;
     instance_main.call(&mut store, ())?;
 
-    Ok(())
+    // Let's try to read the thing
+    let mut output = String::new();
+    capstd_file.seek(std::io::SeekFrom::Start(0))?; // seek to the beginning
+    let stdout_file_handle = CapStdFile::into_std(capstd_file);
+    let mut buf_reader = std::io::BufReader::new(stdout_file_handle);
+    buf_reader.read_to_string(&mut output)?;
+
+    Ok(output)
 }
